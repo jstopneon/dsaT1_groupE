7ca0f38f56421939347ccb3fc30c674a80eb90ef
jstopneon <chu0ting94202@gmail.com>
2025-04-01 01:27:49 +0800

    Delete pages directory

--- pages/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-
-def home_page_view(request):
-    abc = "Welcome to Group E Page!\nGroup Members:\n— 余俊明 (112370117)\n— 朱庭寬 (112370210)\n— 林彥廷 (112370219)\n— 黃昭亮 (112370232)"
-
-    return HttpResponse(abc, content_type="text/plain; charset=utf-8")
